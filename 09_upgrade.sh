95f607c0b30ce538e9be1b384165e503af71fd5b
borysneselovskyi <70379260+borysneselovskyi@users.noreply.github.com>
2025-04-11 14:32:37 +0200

    Update 09_upgrade.sh

--- 09_upgrade.sh
+++ 09_upgrade.sh
@@ -5,6 +5,4 @@ ip=`kubectl get route | grep minio-api | awk '{print $2}'`
 sed -e 's/#IP#/'${ip}'/' cluster-example-upgrade-template.yaml > cluster-example-upgrade.yaml
 printf "${green}kubectl apply -f cluster-example-upgrade.yaml${reset}\n"
 
-diff -a --suppress-common-lines -y cluster-example.yaml cluster-example-upgrade.yaml
-
 kubectl apply -f cluster-example-upgrade.yaml
