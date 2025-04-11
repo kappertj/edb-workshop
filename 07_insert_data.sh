6c89aa8a65c0e8dd793df1f7d085edcfe507af07
borysneselovskyi <70379260+borysneselovskyi@users.noreply.github.com>
2025-04-11 13:46:11 +0200

    Delete 07_insert_data.sh

--- 07_insert_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. ./config.sh
-
-printf "${green}echo \"create table test (id integer, description text); \ninsert into test values (1, 'TEST1');\" | kubectl-cnpg psql  cluster-example${reset}\n"
-echo "create table test (id integer, description text); insert into test values (1, 'TEST1');" | kubectl-cnpg psql  ${cluster_name}
-
-printf "${green} echo \"select * from test;\" | kubectl-cnpg psql  cluster-example${reset}\n"
-echo "select * from test;" | kubectl-cnpg psql  ${cluster_name}
