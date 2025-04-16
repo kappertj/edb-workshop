2251e23c7b27d092d9b12df10abcd55ac0e9cd90
Jan Kappert <jan.kappert@hcs-company.com>
2025-04-16 12:32:59 +0200

    fix path

--- 06_show_status.sh
+++ 06_show_status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH=$PATH:/home/workshop/workshop/bin
+PATH=$PATH:~/bin
 #alias kg='/usr/local/bin/kubectl-cnpg'
 watch -c -n 2 kubectl-cnpg --color always status cluster-example
 #watch -c -n 2 "kubectl-cnpg status cluster-example | tail -20"
